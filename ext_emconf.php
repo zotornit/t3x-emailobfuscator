8d847b73864aebb4ae1d32ed89c2233e5b9b5eb6
Thomas Pronold <mail@tomgrill.de>
2017-10-10 09:23:23 +0200

    Set compatability to TYPO3 7 and 8

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Replaces the default email address spam protection with a better one. The email obfuscation is more randomized, safer and more userfriendly for the website visitor.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '3.0.0',
+	'version' => '3.0.3',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.5.0-7.1.999',
-			'typo3' => '7.6.0-8.7.999',
+			'typo3' => '7.6.0-8.8.99',
 		),
 		'conflicts' => array(
 		),
