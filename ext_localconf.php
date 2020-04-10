bb06d25b1a54f5027a9f7638666b699093deb619
Thomas Pronold <tp@zotorn.de>
2020-04-10 19:33:39 +0200

    Recommended JS fire structure

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,5 +7,5 @@ $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['headerNoCache']['tx_
     = \ZOTORN\EmailObfuscator\Service\CSSService::class . '->addAllowedSelectorsToCSSDefaultStyle';
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-    'page.includeJS.emailobfuscator = EXT:emailobfuscator/Resources/Public/Assets/Javascript/emailobfuscator.js'
+    'page.includeJS.emailobfuscator = EXT:emailobfuscator/Resources/Public/JavaScript/emailobfuscator.js'
 );
