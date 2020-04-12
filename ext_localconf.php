de89c826d74f3784ab457c17a74e843b5065b24f
Thomas Pronold <tp@zotorn.de>
2020-04-12 22:35:19 +0200

    Fix bug where obfuscated content would not get cached

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@
 defined('TYPO3_MODE') || die();
 
 call_user_func(function() {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output']['tx_emailobfuscator']
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all']['tx_emailobfuscator']
         = \EMAILOBFUSCATOR\Emailobfuscator\Hook\ObfuscationHook::class . '->obfuscatePageContent';
 
     $GLOBALS['TYPO3_CONF_VARS']['LOG']['EMAILOBFUSCATOR']['Emailobfuscator']['Hook']['ObfuscationHook']['writerConfiguration'] = [
